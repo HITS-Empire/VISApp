9bf3f65c88ee486aadf8037988576fba8fa825c5
yazloysasha <yazloysasha@gmail.com>
2024-04-23 13:11:24 +0700

    Тестовый коммит

--- app/src/main/java/ru/tsu/visapp/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/ru/tsu/visapp/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Дашборд"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/ru/tsu/visapp/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/ru/tsu/visapp/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Уведомления"
     }
     val text: LiveData<String> = _text
 }
