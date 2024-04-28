d7b5559a69284ca2f93177a4a8ad78d7aa9dec1f
yazloysasha <yazloysasha@gmail.com>
2024-04-28 12:58:15 +0700

    Рефактор + добавлен функционал для кнопки Назад

--- app/src/main/java/ru/tsu/visapp/FiltersActivity.kt
+++ app/src/main/java/ru/tsu/visapp/FiltersActivity.kt
@@ -2,8 +2,11 @@ package ru.tsu.visapp
 
 import android.net.Uri
 import android.os.Bundle
+import android.content.Intent
 import android.content.Context
+import android.widget.TextView
 import android.widget.ImageView
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class FiltersActivity: AppCompatActivity() {
@@ -22,5 +25,21 @@ class FiltersActivity: AppCompatActivity() {
         // Получить картинку и установить её
         val selectedUri = sharedPreferences.getString("selected_uri", "")
         imageView.setImageURI(Uri.parse(selectedUri))
+
+        val backButton: ImageButton = findViewById(R.id.backButton) // Кнопка "Назад"
+        val saveButton: TextView = findViewById(R.id.saveButton) // Кнопка "Сохранить"
+
+        // События кликов по кнопкам
+        backButton.setOnClickListener {
+            goToMainActivity()
+        }
+        saveButton.setOnClickListener {
+            //
+        }
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
