0e554ace64ff7971bc77da05072de9aa1c69f546
vekosek <88042533+VictoriaKlyueva@users.noreply.github.com>
2024-05-01 01:51:18 +0700

    Адаптивная верстка + логика получения и установки изображения

--- app/src/main/java/ru/tsu/visapp/NeuralActivity.kt
+++ app/src/main/java/ru/tsu/visapp/NeuralActivity.kt
@@ -1,14 +1,44 @@
 package ru.tsu.visapp
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import ru.tsu.visapp.utils.ImageEditor
+import ru.tsu.visapp.utils.ImageGetter
 
 /*
  * Экран для нейронной сети
  */
 
 class NeuralActivity: ChildActivity() {
+    private lateinit var imageEditor: ImageEditor // Редактор изображений
+    private lateinit var bitmap: Bitmap // Картинка для редактирования
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initializeView(R.layout.activity_neural)
+
+        ImageGetter(this, null, findFaces)
+    }
+
+    private val findFaces = fun () {
+        setImage()
+
+        // Логика работы нейросети
+    }
+
+    private val setImage = fun() {
+        // Получить редактор изображений
+        imageEditor = ImageEditor(contentResolver)
+
+        // Получить imageView
+        val imageView: ImageView = findViewById(R.id.neuralImageView)
+
+        // Получить картинку и установить её
+        val savedImageUri = imageEditor.getSavedImageUri(this, null)
+        bitmap = imageEditor.createBitmapByUri(savedImageUri)
+        imageView.setImageBitmap(bitmap)
     }
 }
