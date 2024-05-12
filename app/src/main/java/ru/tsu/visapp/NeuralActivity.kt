968f953964b8789c3293afa346a2af549da9d3a8
vekosek <88042533+VictoriaKlyueva@users.noreply.github.com>
2024-05-12 23:26:50 +0700

    Начата работа с нейронной сетью

--- app/src/main/java/ru/tsu/visapp/NeuralActivity.kt
+++ app/src/main/java/ru/tsu/visapp/NeuralActivity.kt
@@ -1,9 +1,12 @@
 package ru.tsu.visapp
 
-import android.os.Bundle
 import android.graphics.Bitmap
+import android.os.Bundle
 import android.widget.ImageView
 import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+import org.opencv.dnn.*
 import ru.tsu.visapp.utils.ImageEditor
 import ru.tsu.visapp.utils.ImageGetter
 
@@ -14,30 +17,35 @@ import ru.tsu.visapp.utils.ImageGetter
 class NeuralActivity: ChildActivity() {
     private val imageEditor = ImageEditor() // Редактор изображений
     private lateinit var bitmap: Bitmap // Картинка для редактирования
+    private lateinit var net: Net
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        OpenCVLoader.initDebug();
+
         initializeView(R.layout.activity_neural)
         imageEditor.contentResolver = contentResolver
 
         ImageGetter(this, null, findFaces)
 
-        OpenCVLoader.initDebug()
+        val path = "C:/Users/Legion/Documents/GitHub/VISApp/app/src/main/resources/neuralNetwork/ssd.onnx"
+        net = Dnn.readNetFromONNX(path)
     }
 
     private val findFaces = fun () {
-        setImage()
-
-        // Логика работы нейросети
-    }
-
-    private val setImage = fun() {
         // Получить imageView
         val imageView: ImageView = findViewById(R.id.neuralImageView)
 
-        // Получить картинку и установить её
+        // Получить картинку
         val savedImageUri = imageEditor.getSavedImageUri(this, null)
         bitmap = imageEditor.createBitmapByUri(savedImageUri)
+
+        // Логика работы нейросети
+        val mat = Mat()
+        Utils.bitmapToMat(bitmap, mat)
+
+        // Установить картинку
         imageView.setImageBitmap(bitmap)
     }
 }
