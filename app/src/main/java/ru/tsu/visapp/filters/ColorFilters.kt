dea5b59f4f16f8eda6154ab53b75f814c9b09e44
vekosek <88042533+VictoriaKlyueva@users.noreply.github.com>
2024-05-15 23:39:00 +0700

    Реализовано изменение контраста

--- app/src/main/java/ru/tsu/visapp/filters/ColorFilters.kt
+++ app/src/main/java/ru/tsu/visapp/filters/ColorFilters.kt
@@ -25,6 +25,7 @@ class ColorFilters {
         for (i in 0 ..< width) {
             for (j in 0 ..< height) {
                 val pixel = pixelsEditor.getPixel(i, j)
+
                 val alpha = pixel?.alpha ?: 0
                 var red = pixel?.red ?: 0
                 var green = pixel?.green ?: 0
@@ -33,6 +34,64 @@ class ColorFilters {
                 val max = if (value < 0) 255 else 128
 
                 val grayScale = (red * 0.2126 + green * 0.7152 + blue * 0.0722).toInt()
+
+                red += (red - grayScale) * value / max;
+                green += (green - grayScale) * value / max;
+                blue += (blue - grayScale) * value / max;
+
+                red = red.coerceIn(0, 255)
+                green = green.coerceIn(0, 255)
+                blue = blue.coerceIn(0, 255)
+
+                pixelsEditorResult.setPixel(i, j, Color.argb(alpha, red, green, blue))
+            }
+        }
+
+        return resultPixels
+    }
+
+    fun contrast(
+        pixels: IntArray,
+        width: Int,
+        height: Int,
+        value: Int
+    ): IntArray {
+        val resultPixels = IntArray(pixels.size) { 0 }
+
+        pixelsEditor = PixelsEditor(pixels, width, height)
+        pixelsEditorResult = PixelsEditor(resultPixels, width, height)
+
+        var grayScale = 0
+
+        // Высчитывание среднего значения серого для всего изображения
+        for (i in 0 ..< width) {
+            for (j in 0 ..< height) {
+                val pixel = pixelsEditor.getPixel(i, j)
+
+                var red = pixel?.red ?: 0
+                var green = pixel?.green ?: 0
+                var blue = pixel?.blue ?: 0
+
+                grayScale += (
+                        red * 0.2126 +
+                        green * 0.7152 +
+                        blue * 0.0722
+                ).toInt()
+            }
+        }
+
+        grayScale /= (width * height)
+
+        for (i in 0 ..< width) {
+            for (j in 0 ..< height) {
+                val pixel = pixelsEditor.getPixel(i, j)
+
+                val alpha = pixel?.alpha ?: 0
+                var red = pixel?.red ?: 0
+                var green = pixel?.green ?: 0
+                var blue = pixel?.blue ?: 0
+
+                val max = if (value < 0) 255 else 128
 
                 red += (red - grayScale) * value / max;
                 green += (green - grayScale) * value / max;
