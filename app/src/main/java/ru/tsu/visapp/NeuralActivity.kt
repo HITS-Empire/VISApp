8a82f5d11e7b5a90ac83b125c9f7426fc86a3154
vekosek <88042533+VictoriaKlyueva@users.noreply.github.com>
2024-05-15 13:52:14 +0700

    Полностью реализована нейросеть

--- app/src/main/java/ru/tsu/visapp/NeuralActivity.kt
+++ app/src/main/java/ru/tsu/visapp/NeuralActivity.kt
@@ -1,21 +1,20 @@
 package ru.tsu.visapp
 
-import java.io.File
+import android.graphics.Bitmap
+import android.graphics.Point
 import android.os.Bundle
+import android.widget.ImageView
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.*
 import org.opencv.dnn.Dnn
 import org.opencv.dnn.Net
-import org.opencv.core.Mat
-import org.opencv.core.Size
-import org.opencv.core.Scalar
 import org.opencv.imgproc.Imgproc
-import android.graphics.Bitmap
-import android.widget.ImageView
-import org.opencv.android.Utils
-import java.io.FileOutputStream
-import java.io.BufferedInputStream
-import org.opencv.android.OpenCVLoader
 import ru.tsu.visapp.utils.ImageEditor
 import ru.tsu.visapp.utils.ImageGetter
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
 
 /*
  * Экран для нейронной сети
@@ -24,7 +23,8 @@ import ru.tsu.visapp.utils.ImageGetter
 class NeuralActivity: ChildActivity() {
     private val imageEditor = ImageEditor() // Редактор изображений
     private lateinit var bitmap: Bitmap // Картинка для редактирования
-    private lateinit var net: Net
+    private lateinit var result: Bitmap // Результирующее изображение
+    private lateinit var net: Net // Нейронная сеть
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,23 +56,59 @@ class NeuralActivity: ChildActivity() {
 
         // Приведение изображения к верному размеру и формату
         Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGBA2RGB)
-
-        val resizedMat = Mat()
-        Imgproc.resize(mat, resizedMat, Size(300.0, 300.0))
+        val frame = Mat()
+        Imgproc.resize(mat, frame, Size(300.0, 300.0))
 
         // Получение blob нового размера и с вычитанием среднего
         val blob = Dnn.blobFromImage(
-            resizedMat,
+            frame,
             1.0,
             Size(300.0, 300.0),
-            Scalar(104.0, 177.0, 123.0)
+            Scalar(104.0, 177.0, 123.0),
+            true,
+            false
         )
 
+        // Установка входных данных в модель
         net.setInput(blob)
 
-        val outputs = net.forward()
+        // Получение и преобразование детектированных объектов
+        var detections = net.forward()
+        detections = detections.reshape(1, detections.total().toInt() / 7)
 
-        println(outputs.size())
+        // Размеры изображения
+        val cols: Int = frame.cols()
+        val rows: Int = frame.rows()
+
+        // Порог уверенности модели в предсказании
+        val threshold = 0.5
+
+        // Отрисовка bounding boxes на изображении
+        for (i in 0 until detections.rows()) {
+            val confidence = detections.get(i, 2)[0]
+            if (confidence > threshold) {
+                val left = (detections.get(i, 3)[0] * cols).toInt()
+                val top = (detections.get(i, 4)[0] * rows).toInt()
+                val right = (detections.get(i, 5)[0] * cols).toInt()
+                val bottom = (detections.get(i, 6)[0] * rows).toInt()
+
+                // Отрисовка прямоугольника вокруг обнаруженного объекта
+                Imgproc.rectangle(
+                    frame,
+                    Point(left.toDouble(), top.toDouble()),
+                    Point(right.toDouble(), bottom.toDouble()),
+                    Scalar(0.0, 255.0, 0.0)
+                )
+            }
+        }
+
+        // Перевести frame в bitmap
+        result = Bitmap.createBitmap(
+            frame.cols(),
+            frame.rows(),
+            Bitmap.Config.ARGB_8888
+        )
+        Utils.matToBitmap(frame, result)
 
         // Установить картинку
         imageView.setImageBitmap(bitmap)
