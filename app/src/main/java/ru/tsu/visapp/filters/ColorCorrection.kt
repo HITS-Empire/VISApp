48d10f20d80fd416e631b29fac0cb6018de26d17
vekosek <88042533+VictoriaKlyueva@users.noreply.github.com>
2024-05-16 13:10:10 +0700

    Убита цветокоррекция

--- app/src/main/java/ru/tsu/visapp/filters/ColorCorrection.kt
+++ app/src/main/java/ru/tsu/visapp/filters/ColorCorrection.kt
@@ -12,112 +12,46 @@ class ColorCorrection {
     private lateinit var pixelsEditorResult: PixelsEditor
     private lateinit var resultPixels: IntArray
 
-    private fun saturation(
-        width: Int,
-        height: Int,
+    private fun brightness(
+        red: Int,
+        green: Int,
+        blue: Int,
         value: Int
-    ) {
-        for (i in 0 ..< width) {
-            for (j in 0 ..< height) {
-                val pixel = pixelsEditor.getPixel(i, j)
+    ): Array<Int> {
+        return arrayOf(red + value, green + value, blue + value)
+    }
 
-                val alpha = pixel?.alpha ?: 0
-                var red = pixel?.red ?: 0
-                var green = pixel?.green ?: 0
-                var blue = pixel?.blue ?: 0
+    private fun saturation(
+        red: Int,
+        green: Int,
+        blue: Int,
+        value: Int
+    ): Array<Int> {
+        val max = if (value < 0) 255 else 128
 
-                val max = if (value < 0) 255 else 128
+        val grayScale = (red * 0.2126 + green * 0.7152 + blue * 0.0722).toInt()
 
-                val grayScale = (red * 0.2126 + green * 0.7152 + blue * 0.0722).toInt()
-
-                red += (red - grayScale) * value / max;
-                green += (green - grayScale) * value / max;
-                blue += (blue - grayScale) * value / max;
-
-                red = red.coerceIn(0, 255)
-                green = green.coerceIn(0, 255)
-                blue = blue.coerceIn(0, 255)
-
-                pixelsEditorResult.setPixel(i, j, Color.argb(alpha, red, green, blue))
-            }
-        }
+        return arrayOf(
+            red + (red - grayScale) * value / max,
+            green + (green - grayScale) * value / max,
+            blue + (blue - grayScale) * value / max
+        )
     }
 
     private fun contrast(
-        width: Int,
-        height: Int,
+        red: Int,
+        green: Int,
+        blue: Int,
+        grayScale: Int,
         value: Int
-    ) {
-        var meanGrayScale = 0
+    ): Array<Int> {
+        val max = if (value < 0) 255 else 128
 
-        // Высчитывание среднего значения серого для всего изображения
-        for (i in 0 ..< width) {
-            for (j in 0 ..< height) {
-                val pixel = pixelsEditor.getPixel(i, j)
-
-                var red = pixel?.red ?: 0
-                var green = pixel?.green ?: 0
-                var blue = pixel?.blue ?: 0
-
-                meanGrayScale += (
-                        red * 0.2126 +
-                        green * 0.7152 +
-                        blue * 0.0722
-                ).toInt()
-            }
-        }
-
-        meanGrayScale /= (width * height)
-
-        for (i in 0 ..< width) {
-            for (j in 0 ..< height) {
-                val pixel = pixelsEditor.getPixel(i, j)
-
-                val alpha = pixel?.alpha ?: 0
-                var red = pixel?.red ?: 0
-                var green = pixel?.green ?: 0
-                var blue = pixel?.blue ?: 0
-
-                val max = if (value < 0) 255 else 128
-
-                red += (red - meanGrayScale) * value / max;
-                green += (green - meanGrayScale) * value / max;
-                blue += (blue - meanGrayScale) * value / max;
-
-                red = red.coerceIn(0, 255)
-                green = green.coerceIn(0, 255)
-                blue = blue.coerceIn(0, 255)
-
-                pixelsEditorResult.setPixel(i, j, Color.argb(alpha, red, green, blue))
-            }
-        }
-    }
-
-    private fun brightness(
-        width: Int,
-        height: Int,
-        value: Int
-    ) {
-        for (i in 0 ..< width) {
-            for (j in 0 ..< height) {
-                val pixel = pixelsEditor.getPixel(i, j)
-
-                val alpha = pixel?.alpha ?: 0
-                var red = pixel?.red ?: 0
-                var green = pixel?.green ?: 0
-                var blue = pixel?.blue ?: 0
-
-                red += value;
-                green += value;
-                blue += value;
-
-                red = red.coerceIn(0, 255)
-                green = green.coerceIn(0, 255)
-                blue = blue.coerceIn(0, 255)
-
-                pixelsEditorResult.setPixel(i, j, Color.argb(alpha, red, green, blue))
-            }
-        }
+        return arrayOf(
+            red + (red - grayScale) * value / max,
+            green + (green - grayScale) * value / max,
+            blue + (blue - grayScale) * value / max
+        )
     }
 
     fun correctColor(
@@ -133,26 +67,66 @@ class ColorCorrection {
         pixelsEditor = PixelsEditor(pixels, width, height)
         pixelsEditorResult = PixelsEditor(resultPixels, width, height)
 
-        // Яркость
-        brightness(
-            width,
-            height,
-            brightnessValue
-        )
+        // Высчитывание среднего значения серого для всего изображения
+        var meanGrayScale = 0
 
-        // Насыщенность
-        saturation(
-            width,
-            height,
-            saturationValue
-        )
+        for (i in 0 ..< width) {
+            for (j in 0 ..< height) {
+                val pixel = pixelsEditor.getPixel(i, j)
 
-        // Контраст
-        contrast(
-            width,
-            height,
-            contrastValue
-        )
+                var red = pixel?.red ?: 0
+                var green = pixel?.green ?: 0
+                var blue = pixel?.blue ?: 0
+
+                meanGrayScale += (red * 0.2126 + green * 0.7152 + blue * 0.0722).toInt()
+            }
+        }
+
+        meanGrayScale /= (width * height)
+
+        for (i in 0 ..< width) {
+            for (j in 0..<height) {
+                val pixel = pixelsEditor.getPixel(i, j)
+
+                val alpha = pixel?.alpha ?: 0
+                var red = pixel?.red ?: 0
+                var green = pixel?.green ?: 0
+                var blue = pixel?.blue ?: 0
+
+                // Яркость
+                val (
+                    brightRed,
+                    brightGreen,
+                    brightBlue
+                ) = brightness(red, green, blue, brightnessValue)
+
+                // Насыщенность
+                val (
+                    saturationRed,
+                    saturationGreen,
+                    saturationBlue
+                ) = saturation(brightRed, brightGreen, brightBlue, saturationValue)
+
+                // Контраст
+                val (
+                    contrastRed,
+                    contrastGreen,
+                    contrastBlue
+                ) = contrast(
+                    saturationRed,
+                    saturationGreen,
+                    saturationBlue,
+                    meanGrayScale,
+                    contrastValue
+                )
+
+                red = contrastRed.coerceIn(0, 255)
+                green = contrastGreen.coerceIn(0, 255)
+                blue = contrastBlue.coerceIn(0, 255)
+
+                pixelsEditorResult.setPixel(i, j, Color.argb(alpha, red, green, blue))
+            }
+        }
 
         return resultPixels
     }
