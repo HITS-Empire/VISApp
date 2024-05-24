85861668ae7fc89abb51a32561ec101860a94383
yazloysasha <yazloysasha@gmail.com>
2024-05-24 16:35:42 +0700

    Добит 3д куб

--- app/src/main/java/ru/tsu/visapp/utils/cube/Helper.kt
+++ app/src/main/java/ru/tsu/visapp/utils/cube/Helper.kt
@@ -1,5 +1,8 @@
 package ru.tsu.visapp.utils.cube
 
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.math.max
 import kotlin.math.min
 import android.graphics.Color
@@ -7,16 +10,40 @@ import androidx.core.graphics.red
 import androidx.core.graphics.blue
 import androidx.core.graphics.green
 import androidx.core.graphics.alpha
-import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.sin
 
 class Helper {
+    // Цвета сторон куба по умолчанию
+    private val colors = arrayOf(
+        Color.argb(255, 43, 203, 17),
+        Color.argb(255, 255, 90, 37),
+        Color.argb(255, 255, 207, 37),
+        Color.argb(255, 255, 37, 103),
+        Color.argb(255, 144, 37, 252),
+        Color.argb(255, 37, 175, 252)
+    )
+
+    // Получить нужный индекс пикселя
+    private fun getIndexOfImage(
+        width: Int,
+        height: Int,
+        i: Int,
+        j: Int,
+        dx: Float,
+        dy: Float
+    ): Int {
+        return (
+            abs(i * cos(dx) - i * sin(dx)).toInt() % width
+        ) + (
+            abs(j * cos(dy) - j * sin(dy)).toInt() % width * height
+        )
+    }
+
     private fun Boolean.toFloat(): Float = if (this) 1.0f else 0.0f
 
     private fun sign(value: Float): Float {
         return (0.0f < value).toFloat() - (value < 0.0f).toFloat()
     }
+
     private fun sign(vec3: Vec3): Vec3 {
         return Vec3(sign(vec3.x), sign(vec3.y), sign(vec3.z))
     }
@@ -24,6 +51,7 @@ class Helper {
     private fun step(edge: Float, value: Float): Float {
         return (value > edge).toFloat()
     }
+
     private fun step(edge: Vec3, vec3: Vec3): Vec3 {
         return Vec3(step(edge.x, vec3.x), step(edge.y, vec3.y), step(edge.z, vec3.z))
     }
@@ -31,6 +59,7 @@ class Helper {
     private fun min(vec3: Vec3): Float {
         return min(min(vec3.x, vec3.y), vec3.z)
     }
+
     private fun max(vec3: Vec3): Float {
         return max(max(vec3.x, vec3.y), vec3.z)
     }
@@ -52,12 +81,12 @@ class Helper {
         imagePixels: Array<IntArray>,
         width: Int,
         height: Int,
-        isTerrible: Boolean,
         i: Int,
         j: Int,
-        dy: Float,
         dx: Float,
+        dy: Float,
         light: Vec3,
+        isTerrible: Boolean
     ): Int? {
         val m = Vec3(1) / beamDirection
         val n = m * cameraPosition
@@ -76,60 +105,29 @@ class Helper {
 
         val normal = -sign(beamDirection) * step(yzx, t1) * step(zxy, t1)
 
-        val newColor = normal.dot(light) * 0.5f + 0.5f
-        //println("$newColor")
+        val newColor = 3.0f / tN // normal.dot(light) * 0.5f + 0.5f
         if (!isTerrible) {
             return when {
-                normal.x == -1.0f -> {
-                    multiplicationColor(imagePixels[0][0], newColor)
-                }
-                normal.y == -1.0f -> {
-                    multiplicationColor(imagePixels[1][0], newColor)
-                }
-                normal.x == 1.0f -> {
-                    multiplicationColor(imagePixels[2][0], newColor)
-                }
-                normal.y == 1.0f -> {
-                    multiplicationColor(imagePixels[3][0], newColor)
-                }
-                normal.z == -1.0f -> {
-                    multiplicationColor(imagePixels[4][0], newColor)
-                }
-                normal.z == 1.0f -> {
-                    multiplicationColor(imagePixels[5][0], newColor)
-                }
+                normal.x == -1.0f -> multiplicationColor(colors[0], newColor)
+                normal.y == -1.0f -> multiplicationColor(colors[1], newColor)
+                normal.x == 1.0f -> multiplicationColor(colors[2], newColor)
+                normal.y == 1.0f -> multiplicationColor(colors[3], newColor)
+                normal.z == 1.0f -> multiplicationColor(colors[4], newColor)
+                normal.z == -1.0f -> multiplicationColor(colors[5], newColor)
                 else -> null
             }
-        } else {
-            return when {
-                t1.x > t1.y && t1.x > t1.z -> {
-                    if (cameraPosition.x < 0) {
-                        imagePixels[0][abs(i * cos(dx) - i * sin(dx)).toInt() % width + abs(
-                            j * cos(dy) - j * sin(dy)
-                        ).toInt() % width * width]
-                    } else {
-                        imagePixels[1][abs(i * cos(dx) - i * sin(dx)).toInt() % width + abs(
-                            j * cos(dy) - j * sin(dy)
-                        ).toInt() % width * width]
-                    }
-                }
-                t1.y > t1.x && t1.y > t1.z -> {
-                    if (cameraPosition.y < 0) imagePixels[2][abs(i * cos(dx) - i * sin(dx)).toInt() % width + abs(
-                        j * cos(dy) - j * sin(dy)
-                    ).toInt() % width * width]
-                    else {
-                        imagePixels[3][abs(i * cos(dx) - i * sin(dx)).toInt() % width + abs(
-                            j * cos(dy) - j * sin(dy)
-                        ).toInt() % width * width]
-                    }
-                }
-                cameraPosition.z < 0 -> imagePixels[4][abs(i * cos(dx) - i * sin(dx)).toInt() % width + abs(
-                    j * cos(dy) - j * sin(dy)
-                ).toInt() % width * width]
-                else -> imagePixels[5][abs(i * cos(dx) - i * sin(dx)).toInt() % width + abs(
-                    j * cos(dy) - j * sin(dy)
-                ).toInt() % width * width]
-            }
+        }
+
+        val index = getIndexOfImage(width, height, i, j, dx, dy)
+
+        return when {
+            normal.x == -1.0f -> multiplicationColor(imagePixels[0][index], newColor)
+            normal.y == -1.0f -> multiplicationColor(imagePixels[1][index], newColor)
+            normal.x == 1.0f -> multiplicationColor(imagePixels[2][index], newColor)
+            normal.y == 1.0f -> multiplicationColor(imagePixels[3][index], newColor)
+            normal.z == 1.0f -> multiplicationColor(imagePixels[4][index], newColor)
+            normal.z == -1.0f -> multiplicationColor(imagePixels[5][index], newColor)
+            else -> null
         }
     }
 }
